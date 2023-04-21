3c86ddf1a813b435c8bed23e476ba4bf69520f15
stvjbs <yarashukartem@icloud.com>
2023-04-21 13:35:30 +0300

    HW 4 ready

--- Program.cs
+++ Program.cs
@@ -1,58 +1,50 @@
-﻿// Homework lesson 3
+﻿// Homework lesson 4
 
-// Task 1 Напишите программу, которая принимает на
-// вход ?пятизначное? число и проверяет, является ли оно палиндромом.
-
-// bool Pal(int num)
+// Task 1 Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// int NumAB(int a, int b)
 // {
-//  int current = num;
-//  string num2 = "";
-//  while(current>=1)
-//  {
-//     num2 = num2 + (current % 10);
-//     current = current / 10;
-//  }   
-//  int numcheck = Convert.ToInt32(num2);
-//  if(num == numcheck) return true;
-//  else return false;
-// }
-// Console.WriteLine("Input number");
-// int num1 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine(Pal(num1) ? "Yes" : "No");
-
-// Task 2 Напишите программу, которая принимает на вход координаты двух точек
-// и находит расстояние между ними в 3D пространстве.
-
-// double Length3D(double x1, double y1,double z1,double x2,double y2,double z2)
-// {
-//     double length = Math.Round(Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)+Math.Pow((z2-z1),2)),2);
-//     return length;
-// }
-// Console.WriteLine("Input coordinate X of the first point");
-// double x1 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Input coordinate Y of the first point");
-// double y1 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Input coordinate Z of the first point");
-// double z1 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Input coordinate X of the second point");
-// double x2 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Input coordinate Y of the second point");
-// double y2 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Input coordinate Z of the second point");
-// double z2 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine($"Length between two drops is {Length3D(x1,y1,z1,x2,y2,z2)}");
-
-// Task 3 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-
-// void TableCubes(int n)
-// {
-// int current = 1; 
-//     while(current <= n)
+//     int result = 1;
+//     for (int c = 1; c <= b; c++)
 //     {
-//         Console.WriteLine(current + "-" + (Math.Pow(current,3)));
-//         current++;
+//         result = result * a;
 //     }
+//     return result;
 // }
-// Console.WriteLine("Input number");
-// int n = Convert.ToInt32(Console.ReadLine());
-// TableCubes(n);
+// Console.WriteLine("Input number A");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input number B");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Your result is - " + NumAB(a,b));
+
+// Task 2 Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// int SumOfNums(int num)
+// {
+//     int sum = 0;
+//     while(num >= 1)
+//     {
+//         sum = sum + (num % 10);
+//         num = num / 10;
+//     }
+//     return sum;
+// }
+// Console.WriteLine("Input number ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Your result is - " + SumOfNums(b));
+
+// Task 3 Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// void ShowArray(int[] array)
+// {
+//     for(int i = 0; i < array.Length; i++)
+//         Console.Write(array[i] + " ");
+
+//     Console.WriteLine();
+// }
+
+// int[] myArray = new int[8];
+// for(int i = 0; i < 8; i++)
+// {
+//     Console.WriteLine("Input element");
+//     myArray[i] = Convert.ToInt32(Console.ReadLine());
+// }
+// ShowArray(myArray);
